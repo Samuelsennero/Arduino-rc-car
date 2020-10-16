dbe0ac4298a403e2d41aad755cb490d42f6ff535
SamuelSennero <samuel.sennero@gmail.com>
2020-10-16 14:10:53 +0200

    2 motors

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,60 @@
 #include <Arduino.h>
-
- int motorPin3  = 10; // Pin  7 of L293
- int motorPin4  = 9;  // Pin  2 of L293
+ // Motor 1
+ int motorPin1  = 5; // Pin 14 of H-bridge
+ int motorPin2  = 6; // Pin 10 of H-bridge
+ // Motor 2
+ int motorPin3  = 10; // Pin  7 of H-bridge
+ int motorPin4  = 9;  // Pin  2 of H-bridge
 
 void setup(){
   
+    pinMode(motorPin1, OUTPUT);
+    pinMode(motorPin2, OUTPUT);
     pinMode(motorPin3, OUTPUT);
     pinMode(motorPin4, OUTPUT);
     Serial.begin(9600);
 }
 
+// Motor 1
+void Motor1Clockwise() {
+   digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+}
 
-void clockwise() {
+void Motor1CounterClockwise() {
+   digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+}
+
+
+// Motor 2
+void Motor2Clockwise() {
    digitalWrite(motorPin3, HIGH);
   digitalWrite(motorPin4, LOW);
-
 }
 
-void counter_clockwise() {
+void Motor2CounterClockwise() {
    digitalWrite(motorPin3, LOW);
   digitalWrite(motorPin4, HIGH);
-
 }
 
-void stop() {
-   digitalWrite(motorPin3, LOW);
+void stopMotors() {
+   digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
   digitalWrite(motorPin4, LOW);
-
 }
+
+// loop
 
 void loop(){
-  clockwise();
+  Motor1Clockwise();
+  Motor2Clockwise();
   delay(4000);
-  counter_clockwise();
+  Motor1CounterClockwise();
+  Motor2CounterClockwise();
   delay(4000);
-
+  
+  
 }
 
